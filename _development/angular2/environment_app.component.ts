90dee11e4c4a000040206e341da0251f4c5a7cb8
Ross Lehr <itsme@rosslehr.com>
2017-04-03 19:06:59 -0400

    initial upload

---
+++ _development/angular2/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<h2>Angular 2 working enviorment!!!!!!!</h2>'
+})
+
+export class AppComponent {
+
+}
